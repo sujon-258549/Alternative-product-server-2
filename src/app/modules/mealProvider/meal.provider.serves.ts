e0727e75114e6b75956716e9a744ec99d850e4e8
Md Sujon Mia <sujon258549@gmail.com>
2025-04-06 20:03:18 +0600

    modified meal.provider.serves.ts in mealProvider

---
+++ src/app/modules/mealProvider/meal.provider.serves.ts
@@ -0,0 +1,9 @@
+import { TMaleProvider } from './meal.provider.interfaces';
+import MaleProvider from './meal.provider.mode';
+
+const CreateMealProviderIntoDB = async (payload: TMaleProvider) => {
+  const result = await MaleProvider.create(payload);
+  return result;
+};
+
+export const mealProvider = { CreateMealProviderIntoDB };
